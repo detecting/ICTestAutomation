360d69efa5f78687d68216c0aebaa82ed129bb96
Morgan Zhang <detect.z@gmail.com>
2018-05-21 16:43:13 +1200

    Add files via upload

---
+++ ICTestAutomation/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ICTestAutomation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        private string url = "https://www.google.com";
+        private static IWebDriver driver;
+        private string inputID = "lst-ib";
+        private string inputKeywords = "github";
+        private string gitUrl = "https://github.com/";
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            driver = new ChromeDriver();
+
+        }
+        [TestMethod]
+        public void GotoUrl()
+        {
+            driver.Navigate().GoToUrl(url);
+            //location
+            IWebElement inputBoxElement = driver.FindElement(By.Id(inputID));
+            //input
+            inputBoxElement.SendKeys(inputKeywords);
+            //enter
+            inputBoxElement.SendKeys(Keys.Enter);
+
+            IWebElement webElement =
+                driver.FindElement(By.LinkText("The world's leading software development platform · GitHub"));
+            string getUrl = webElement.GetAttribute("href");
+
+            Assert.IsTrue(getUrl == gitUrl);
+
+        }
+
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            //driver.Close();
+        }
+
+    }
+}
